16db51c2bcfe93a662ccbff3e4709175b314b4b8
Dang Quoc Huy <huysamdua@yahoo.com>
2018-04-13 19:00:35 +0700

    book a call

---
+++ book_a_call_files/loader-v2.js
@@ -0,0 +1,96 @@
+(function() {
+
+    var __hs_cta_json = {"css":"a#cta_button_443262_98e957c4-e335-4971-84e9-2d0c8cb13c84 {\n  -webkit-font-smoothing:antialiased !important; \n  cursor:pointer !important; \n  -moz-user-select:none !important; \n  -webkit-user-select:none !important; \n  -o-user-select:none !important; \n  user-select:none !important; \n  display:inline-block !important; \n  font-weight:bold !important; \n  text-align:center !important; \n  text-decoration:none !important; \n  font-family:sans-serif !important; \n  border-radius:6px !important; \n  background-color:rgb(235,135,12) !important; \n  background-image:-webkit-linear-gradient(top, rgb(235,135,12), rgb(176,101,9)) !important; \n  background-image :-moz-linear-gradient(top, rgb(235,135,12), rgb(176,101,9)) !important; \n  box-shadow:inset 0px 1px rgb(255,168,15) !important; \n  -webkit-box-shadow:inset 0px 1px rgb(255,168,15) !important; \n  -moz-box-shadow:inset 0px 1px rgb(255,168,15) !important; \n  color:rgb(255, 255, 255) !important; \n  border:2px solid rgb(117,67,6) !important; \n  text-shadow:none !important; \n  line-height:1.5em !important; \n  padding:6px 18px !important; \n}\na#cta_button_443262_98e957c4-e335-4971-84e9-2d0c8cb13c84:hover {\nbackground-color:rgb(255,148,13) !important; \nbackground-image:-webkit-linear-gradient(top, rgb(255,148,13), rgb(176,101,9)) !important; \nbackground-image :-moz-linear-gradient(top, rgb(255,148,13), rgb(176,101,9)) !important; \nbox-shadow:inset 0px 1px rgb(255,182,16), 0px 1px 8px rgba(0, 0, 0, 0.3) !important; \n-webkit-box-shadow:inset 0px 1px rgb(255,182,16), 0px 1px 8px rgba(0, 0, 0, 0.3) !important; \n-moz-box-shadow:inset 0px 1px rgb(255,182,16), 0px 1px 8px rgba(0, 0, 0, 0.3) !important; \ncolor:rgb(255,255,255) !important; \nborder:2px solid rgb(141,81,7) !important; \n}\na#cta_button_443262_98e957c4-e335-4971-84e9-2d0c8cb13c84:active, #cta_button_443262_98e957c4-e335-4971-84e9-2d0c8cb13c84:active:hover {\nbackground-color:rgb(176,101,9) !important; \nbackground-image:-webkit-linear-gradient(top, rgb(176,101,9), rgb(235,135,12)) !important; \nbackground-image :-moz-linear-gradient(top, rgb(176,101,9), rgb(235,135,12)) !important; \nbox-shadow:inset 0px 1px 10px rgba(0, 0, 0, 0.5) !important; \n-webkit-box-shadow:inset 0px 1px 10px rgba(0, 0, 0, 0.5) !important; \n-moz-box-shadow:inset 0px 1px 10px rgba(0, 0, 0, 0.5) !important; \ncolor:rgb(244,244,244) !important; \n}\n\n","image_html":"<a id=\"cta_button_443262_98e957c4-e335-4971-84e9-2d0c8cb13c84\" class=\"cta_button\" href=\"https://www.tinypulse.com/cs/c/?cta_guid=98e957c4-e335-4971-84e9-2d0c8cb13c84&placement_guid=7d1d3f76-2e7c-4a93-9589-2dda2fea4076&portal_id=443262&redirect_url=APefjpH05cKmRDSiyo2iR58z84eHGv_mZMLlw4HiPxANzBO0EY6tDKJo-flN5Tqk761Nut65HG_Roa9a65C752dtZKolF0n8w8WKrzBgtXgYVyBJvnw1WXNRVs97k1R8JtH4Dsypqg3p5kRZqGyIUQpZqvy82QZrLpV9gX1Pc9JF4jCghkaHuo49iWVi8boLDMBiV3O5toLQ25uefZ_TWRvg1NZBaNycXv0Hur59anAlA2xHEAW8PivKv57cNYJ1yt0N4HMKZgrmKTtghkCLFI3JPbjC0I4tPeByKngZJUMQnJdReZClauA&hsutk=9f73e6f49998adfc40fa8a9bab51154b&canon=https%3A%2F%2Fwww.tinypulse.com%2Fblog%2Fhow-pulse-surveys-improve-glassdoor-scores&click=663630c2-d7ab-47d1-8c45-5e1eceff31b6&utm_referrer=https%3A%2F%2Fwww.tinypulse.com%2Fblog&pageId=4958890148\"  cta_dest_link=\"https://www.tinypulse.com/the-ceos-guide-to-glassdoor\"><img id=\"hs-cta-img-7d1d3f76-2e7c-4a93-9589-2dda2fea4076\" class=\"hs-cta-img \" style=\"border-width: 0px; /*hs-extra-styles*/\" mce_noresize=\"1\" alt=\"New Call-to-action\" src=\"https://www.tinypulse.com/hubfs/CEOs_Guide_to_Glassdoor_CTA.png?t=1523576063273\" /></a>","is_image":true,"placement_element_class":"hs-cta-7d1d3f76-2e7c-4a93-9589-2dda2fea4076","raw_html":"<a id=\"cta_button_443262_98e957c4-e335-4971-84e9-2d0c8cb13c84\" class=\"cta_button \" href=\"https://www.tinypulse.com/cs/c/?cta_guid=98e957c4-e335-4971-84e9-2d0c8cb13c84&placement_guid=7d1d3f76-2e7c-4a93-9589-2dda2fea4076&portal_id=443262&redirect_url=APefjpH05cKmRDSiyo2iR58z84eHGv_mZMLlw4HiPxANzBO0EY6tDKJo-flN5Tqk761Nut65HG_Roa9a65C752dtZKolF0n8w8WKrzBgtXgYVyBJvnw1WXNRVs97k1R8JtH4Dsypqg3p5kRZqGyIUQpZqvy82QZrLpV9gX1Pc9JF4jCghkaHuo49iWVi8boLDMBiV3O5toLQ25uefZ_TWRvg1NZBaNycXv0Hur59anAlA2xHEAW8PivKv57cNYJ1yt0N4HMKZgrmKTtghkCLFI3JPbjC0I4tPeByKngZJUMQnJdReZClauA&hsutk=9f73e6f49998adfc40fa8a9bab51154b&canon=https%3A%2F%2Fwww.tinypulse.com%2Fblog%2Fhow-pulse-surveys-improve-glassdoor-scores&click=663630c2-d7ab-47d1-8c45-5e1eceff31b6&utm_referrer=https%3A%2F%2Fwww.tinypulse.com%2Fblog&pageId=4958890148\"  style=\"/*hs-extra-styles*/\" cta_dest_link=\"https://www.tinypulse.com/the-ceos-guide-to-glassdoor\" title=\"New Call-to-action\"><span style=\"color: #ffffff; font-size: 24px; font-family: sans-serif; line-height: 1.5em;\">New Call-to-action</span></a>"};
+    var __hs_cta = {};
+
+    __hs_cta.drop = function() {
+        var is_legacy = document.getElementById('hs-cta-ie-element') && true || false,
+            html = __hs_cta_json.image_html,
+            tags = __hs_cta.getTags(),
+            is_image = __hs_cta_json.is_image,
+            parent, _style;
+
+        if (!is_legacy && !is_image) {
+            parent = (document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]);
+
+            _style = document.createElement('style');
+            parent.insertBefore(_style, parent.childNodes[0]);
+            try {
+                default_css = ".hs-cta-wrapper p, .hs-cta-wrapper div { margin: 0; padding: 0; }";
+                cta_css = default_css + " " + __hs_cta_json.css;
+                // http://blog.coderlab.us/2005/09/22/using-the-innertext-property-with-firefox/
+                _style[document.all ? 'innerText' : 'textContent'] = cta_css;
+            } catch (e) {
+                // addressing an ie9 issue
+                _style.styleSheet.cssText = cta_css;
+            }
+
+            html = __hs_cta_json.raw_html;
+        }
+
+        for (var i = 0; i < tags.length; ++i) {
+
+            var tag = tags[i];
+            var images = tag.getElementsByTagName('img');
+            var cssText = "";
+            var align = "";
+            for (var j = 0; j < images.length; j++) {
+                align = images[j].align;
+                images[j].style.border = '';
+                images[j].style.display = '';
+                cssText = images[j].style.cssText;
+            }
+
+            if (align == "right") {
+                tag.style.display = "block";
+                tag.style.textAlign = "right";
+            } else if (align == "middle") {
+                tag.style.display = "block";
+                tag.style.textAlign = "center";
+            }
+
+            tag.innerHTML = html.replace('/*hs-extra-styles*/', cssText);
+            tag.style.visibility = 'visible';
+            tag.setAttribute('data-hs-drop', 'true');
+            window.hbspt && hbspt.cta && hbspt.cta.afterLoad && hbspt.cta.afterLoad('7d1d3f76-2e7c-4a93-9589-2dda2fea4076');
+        }
+
+        return tags;
+    };
+
+    __hs_cta.getTags = function() {
+        var allTags, check, i, divTags, spanTags,
+            tags = [];
+            if (document.getElementsByClassName) {
+                allTags = document.getElementsByClassName(__hs_cta_json.placement_element_class);
+                check = function(ele) {
+                    return (ele.nodeName == 'DIV' || ele.nodeName == 'SPAN') && (!ele.getAttribute('data-hs-drop'));
+                };
+            } else {
+                allTags = [];
+                divTags = document.getElementsByTagName("div");
+                spanTags = document.getElementsByTagName("span");
+                for (i = 0; i < spanTags.length; i++) {
+                    allTags.push(spanTags[i]);
+                }
+                for (i = 0; i < divTags.length; i++) {
+                    allTags.push(divTags[i]);
+                }
+
+                check = function(ele) {
+                    return (ele.className.indexOf(__hs_cta_json.placement_element_class) > -1) && (!ele.getAttribute('data-hs-drop'));
+                };
+            }
+            for (i = 0; i < allTags.length; ++i) {
+                if (check(allTags[i])) {
+                    tags.push(allTags[i]);
+                }
+            }
+        return tags;
+    };
+
+    // need to do a slight timeout so IE has time to react
+    setTimeout(__hs_cta.drop, 10);
+    window._hsq = window._hsq || [];
+    window._hsq.push(['trackCtaView', '7d1d3f76-2e7c-4a93-9589-2dda2fea4076', '98e957c4-e335-4971-84e9-2d0c8cb13c84']);
+}());
